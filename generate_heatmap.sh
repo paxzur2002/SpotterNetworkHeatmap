0bf58d33dd9d05aeea448b8af3720f35bacb6d97
paxzur2002 <101949087+paxzur2002@users.noreply.github.com>
2023-05-09 18:16:22 -0400

    Add generate_heatmap.sh
    Adds the script to take all these scripts, run them one by one, so that they can generate the heatmap HTML file under the filename spotternetwork_heatmap_current.html!

---
+++ generate_heatmap.sh
@@ -0,0 +1,4 @@
+sh save_current_spotternetwork_db.sh
+cat tmp/spotternetwork_db.txt | sh awk_filter.sh > coordinates.csv
+cat coordinates.csv | grep -Eo '^[-0-9.]+,[-0-9.]+' > latlon.csv
+/usr/local/opt/python@3.10/bin/python3.10 heatmap.py
